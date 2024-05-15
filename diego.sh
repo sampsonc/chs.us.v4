8d1a0d33fe2b78c909fe77fa1c57b391efe236a0
Carl Sampson <carl.sampson@gmail.com>
2024-05-14 22:07:56 -0400

    Content

--- diego.sh
+++ diego.sh
@@ -1,1 +1,3 @@
-diego i g library -i csv/goodreads_library_export.csv --all --scrape --shortcode
+#!/bin/zsh
+cd /Users/chs/Documents/chs.us.v3
+diego i g library -i /Users/chs/Documents/chs.us.v3/csv/goodreads_library_export.csv --all --scrape --shortcode --overwrite
